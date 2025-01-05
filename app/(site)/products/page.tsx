5233c3401b3835877075488e5c52435722605a06
komeyl.al <komeyl.al@gmail.com>
2025-01-05 21:54:21 +0330

    server side fixed

--- app/(site)/products/page.tsx
+++ app/(site)/products/page.tsx
@@ -1,44 +1,39 @@
-import Header from '@/components/Header'
-import ShopComp from '@/components/ShopComp'
-import axios from 'axios'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import Header from '@/components/Header';
+import ShopComp from '@/components/ShopComp';
+import axios from 'axios';
 
 export default async function Products() {
+  try {
+    const [productsRes, catsRes] = await Promise.all([
+      axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}products`),
+      axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}product/categories`),
+    ]);
 
-  let products: any
-    await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}products`)
-      .catch(function (error) {
-        console.log(error)
-        return redirect('/server-error')
-      })
-      .then(function (response) {
-        products = response.data
-      })
+    const products = productsRes.data || [];
+    const cats = catsRes.data || [];
 
-  let cats: any    
-    await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}product/categories`)
-      .then(function (response) {
-        cats = response.data
-      })
-      .catch(function (error) {
-        console.log(error)
-        return redirect('/server-error')
-      })
+    if (products.length === 0) {
+      return (
+        <div className="p-8">
+          <Header title="محصولات" />
+          <p className="text-center p-10">هنوز محصولی موجود نیست</p>
+        </div>
+      );
+    }
 
-  if (products.length === 0) {
     return (
-      <div className='p-8'>
-        <Header title='محصولات' />
-        <p className='text-center p-10'>هنوز محصولی موجود نیست</p>
+      <div className="py-8">
+        <Header title="محصولات" />
+        <ShopComp data={products} cats={cats} />
       </div>
-    )
+    );
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return (
+      <div className="p-8">
+        <Header title="خطا" />
+        <p className="text-center p-10">مشکلی در دریافت اطلاعات رخ داده است.</p>
+      </div>
+    );
   }
-
-  return (
-    <div className='py-8'>
-      <Header title='محصولات' />
-      <ShopComp data={products} cats={cats} />
-    </div>
-  )
 }
